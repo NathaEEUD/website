711c1926ec6a194ac23dcad85ea0d9943a61c4eb
Nathaly <natharb09@gmail.com>
2021-11-14 17:01:02 -0500

    extending chakra-ui theme

---
+++ components/00_foundations/theme.ts
@@ -0,0 +1,28 @@
+import { ColorModeOptions, extendTheme, theme } from '@chakra-ui/react'
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+
+const config: ColorModeOptions = {
+  useSystemColorMode: true
+}
+
+const breakpoints = createBreakpoints({
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px'
+})
+
+export default extendTheme({
+  config,
+  breakpoints,
+  styles: {
+    global: {
+      '*, *:before, *:after': {
+        boxSizing: 'border-box'
+      },
+      'html, body': {
+        boxSizing: 'border-box'
+      }
+    }
+  }
+})
