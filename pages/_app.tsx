34bc3d7b1a0d134ce98672ab7dcf35b023df3710
Nathaly <natharb09@gmail.com>
2021-11-15 09:58:34 -0500

    setting up the chakra provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@foundations'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}><Component {...pageProps} /></ChakraProvider>
 }
 
 export default MyApp
